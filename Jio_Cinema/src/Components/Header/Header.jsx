d4088396b81ea643c28c17b37eea42777ebc100f
Aamir <aamir557ahmad@gmail.com>
2025-03-13 20:01:27 +0530

    Jio Cinema App Added

--- Jio_Cinema/src/Components/Header/Header.jsx
+++ Jio_Cinema/src/Components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './header.css'
 import JClogo from '../../assets/jc_logo_v2.svg'
+import crown from '../../assets/crown.svg'
+import { ChevronDown } from 'lucide-react';
 
 const Header = () => {
+
+
     return (
         <>
             <header className="header">
@@ -10,11 +14,22 @@ const Header = () => {
                 <nav className='navigation'>
                     <div className='logo'>
                         <img src={JClogo} alt='JCLOGO' />
-                        <div className='premium'>Go Premium</div>
+                        <div className='premium'> <img src={crown} alt="crown" />
+                            <p>Go Premium</p>
+                        </div>
                     </div>
+
+                    <ul className='navLinks'>
+                        <li className='link'>Home</li>
+                        <li className='link'>Sports</li>
+                        <li className='link'>Movies</li>
+                        <li className='link'>TV Shows</li>
+                        <li className='link'>More </li><ChevronDown />
+                    </ul>
                 </nav>
 
                 <div className='search'>
+                    <div></div>
                 </div>
             </header>
         </>
