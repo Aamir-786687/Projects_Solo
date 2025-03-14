6cc949acc00cd0414a49344dfcd645840e5a8248
Aamir <aamir557ahmad@gmail.com>
2025-03-14 21:44:14 +0530

    Jio Cinema App Added

--- Jio_Cinema/src/pages/Home.jsx
+++ Jio_Cinema/src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
 import Tags from '../Components/Tags/Tags'
+import Channel from '../Components/Channels/Channel'
+import Banner from '../Components/Banner/Banner'
 
 const Home = () => {
   return (
     <>
     <Header/>
     <Tags />
+    <Banner />
+    <Channel/>
+    
     </>
   )
 }
